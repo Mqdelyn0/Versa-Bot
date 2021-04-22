a4aca7d2a03d4684bb42c0a56ac859684d148bcf
kashike1 <actualronski@gmail.com>
2021-04-22 13:39:47 -0700

    Fix Leave Message

--- src/events/member/guildMemberRemove.js
+++ src/events/member/guildMemberRemove.js
@@ -14,7 +14,7 @@ module.exports = async(client, member) => {
     const message_embed = new Discord.MessageEmbed()
         .setTitle(`Leave [${guild.memberCount}]`)
         .setThumbnail(member.user.avatarURL())
-        .setDescription(`Goodbye ${member.user.tag}! We hope to see you again here on ${gulid.name}!`)
+        .setDescription(`Goodbye ${member.user.tag}! We hope to see you again here on ${guild.name}!`)
         .setFooter(config.BOT_SETTINGS.EMBED_AUTHOR)
         .setColor(config.BOT_SETTINGS.EMBED_COLORS.MAIN);
     channel.send(message_embed);
